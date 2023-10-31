509542c362e9ee0c98c4b76b56b5679b0c5696ae
NoumaanAhamed <noumaanahamed@gmail.com>
2023-10-31 21:50:55 +0530

    base;

---
+++ index.js
@@ -0,0 +1,48 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const {OpenAI } = require("openai");
+require('dotenv').config();
+const client = new Client();
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (msg) => {
+    if (msg.body === 'ping') {
+        await msg.reply('pong');
+    }
+});
+
+client.initialize();
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+const openai = new OpenAI(
+    {
+        apiKey: process.env.OPENAI_API_KEY,
+    }
+);
+
+client.on('message', message => {
+    console.log(message.body);
+
+    if(message.body.startsWith("#")) {
+        runCompletion(message.body.substring(1)).then(result => message.reply(result));
+    }
+});
+
+async function runCompletion(msg) {
+    const completion = await openai.chat.completions.create({
+      prompt:msg,
+      model: "gpt-3.5-turbo",
+      
+    });
+
+    return completion.data.choices[0].text;
+}
